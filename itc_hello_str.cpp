42a74603225cbc7d0bf5a06c46f2548702afd4cd
MrRaib <92303543+MrRaib@users.noreply.github.com>
2021-10-15 10:26:06 +0300

    Add files via upload

---
+++ itc_hello_str.cpp
@@ -0,0 +1,6 @@
+#include "str_easy.h"
+
+void itc_hello_str(string name)
+{
+    cout << "hello, " << name << endl;
+}
---
+++ str_easy.h
@@ -0,0 +1,10 @@
+#ifndef STR_EASY_H_INCLUDED
+#define STR_EASY_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+
+void itc_hello_str(string name);
+
+
+#endif // STR_EASY_H_INCLUDED
